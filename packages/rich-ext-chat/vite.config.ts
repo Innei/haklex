d8edc8fa3c595fe45d22154b7598eaa0e8a34a81
Innei <tukon479@gmail.com>
2026-04-29 21:21:27 +0800

    feat(rich-ext-chat): scaffold package

---
+++ packages/rich-ext-chat/vite.config.ts
@@ -0,0 +1,9 @@
+import { createViteConfig } from '../vite.shared';
+
+export default createViteConfig({
+  vanillaExtract: true,
+  entry: {
+    index: 'src/index.ts',
+    static: 'src/static.ts',
+  },
+});
