472f1c1d18843566024b33cd94169371b8f6fa87
Innei <tukon479@gmail.com>
2026-04-29 22:02:33 +0800

    feat(rich-ext-chat): public exports

--- packages/rich-ext-chat/src/index.ts
+++ packages/rich-ext-chat/src/index.ts
@@ -1,1 +1,20 @@
-export {};
+import './styles.css';
+
+export type { ChatEditDecoratorProps } from './ChatEditDecorator';
+export { ChatEditDecorator } from './ChatEditDecorator';
+export type { ChatEditorModalProps } from './ChatEditorModal';
+export { ChatEditorModal } from './ChatEditorModal';
+export type { ChatEditRendererProps } from './ChatEditRenderer';
+export { ChatEditRenderer } from './ChatEditRenderer';
+export { ChatRenderer } from './ChatRenderer';
+export { chatEditNodes, chatNodes } from './nodes';
+export { $createChatEditNode, $isChatEditNode, ChatEditNode } from './nodes/ChatEditNode';
+export { $createChatNode, $isChatNode, ChatNode } from './nodes/ChatNode';
+export type {
+  ChatMessage,
+  ChatParticipant,
+  ChatParticipantKind,
+  ChatRendererProps,
+  ChatVariant,
+  SerializedChatNode,
+} from './types';
--- packages/rich-ext-chat/src/static.ts
+++ packages/rich-ext-chat/src/static.ts
@@ -1,1 +1,16 @@
-export {};
+import type { Klass, LexicalNode } from 'lexical';
+
+import { ChatNode } from './nodes/ChatNode';
+
+export { ChatRenderer } from './ChatRenderer';
+export type { SerializedChatNode } from './nodes/ChatNode';
+export { $createChatNode, $isChatNode, ChatNode } from './nodes/ChatNode';
+export type {
+  ChatMessage,
+  ChatParticipant,
+  ChatParticipantKind,
+  ChatRendererProps,
+  ChatVariant,
+} from './types';
+
+export const chatNodes: Array<Klass<LexicalNode>> = [ChatNode];
